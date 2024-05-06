decd5c3ef51bde62e35a22531e6c3936cd12fc10
Jordan <jordan.kelloway@keyin.com>
2024-05-06 11:53:28 -0230

    Added users login/ signup

--- src/main/java/org/example/ThoughtTacker.java
+++ src/main/java/org/example/ThoughtTacker.java
@@ -2,11 +2,12 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ThoughtTacker {
 
     public static void main(String[] args) {
