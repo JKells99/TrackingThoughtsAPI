4fb2d545c20f13e700b46c4b456c48eb4fc0386d
Jordan <jordan.kelloway@keyin.com>
2024-05-16 10:05:23 -0230

    Added the ability to generate a token now from the server

--- src/main/java/org/example/controllers/UserController.java
+++ src/main/java/org/example/controllers/UserController.java
@@ -1,14 +1,21 @@
 package org.example.controllers;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.example.entities.LoginCreds;
 import org.example.entities.Thought;
 import org.example.entities.User;
+import org.example.jwt.JwtResponse;
+import org.example.jwt.JwtSecretKeyGenerator;
 import org.example.logger.AppLogger;
 import org.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @CrossOrigin
 @RestController
@@ -17,6 +24,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Value("${jwt.expirationMs}")
+    private long jwtExpirationMs;
 
     @PostMapping("/signup")
     public ResponseEntity<User> addANewThought(@RequestBody User user){
@@ -42,12 +52,24 @@ public class UserController {
     public ResponseEntity<?> login(@RequestBody LoginCreds loginCreds){
 
         if(userService.loginUser(loginCreds.getUsername(),loginCreds.getPassword())){
-            return  ResponseEntity.ok("Login Good To go");
+            String username = loginCreds.getUsername();
+            String token = generateJwtToken(username);
+            return  ResponseEntity.ok(new JwtResponse(token));
         } else{
             return  ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Username Or Password Is Incorrect/User Not Found");
         }
 
 
+
+    }
+
+    private String generateJwtToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS512,JwtSecretKeyGenerator.generateJwtSecretKey())
+                .compact();
     }
 
 
